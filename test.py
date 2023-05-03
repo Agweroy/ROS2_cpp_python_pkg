9f2ed8e1c1bdf46f2d2210ad8f27477f6d263daa
Agweroy <agweagwe45@gmail.com>
2023-05-03 15:09:35 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+from turtle import Turtle,Screen
+
+roy = Turtle()
+roy.shape("turtle")
+roy.color("coral")
+roy.circle(150,7200)
+#roy.forward(100)
+MyScreen = Screen()
+MyScreen.exitonclick()
+print(roy)
+    
